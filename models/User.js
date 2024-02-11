cf8ff1236b53d7044d31682df788635480596425
Joseph Campbell <campbellbjoseph@gmail.com>
2024-02-11 00:58:49 -0500

    Added name, email, password, desc, listing, and internship to userSchema in User.js

--- models/User.js
+++ models/User.js
@@ -1,1 +1,44 @@
 // User using the site (includes name, email, password, description, etc.)
+import { Listing } from "./Listing.js";
+import { Internship } from "./Internship.js"
+import { Conversation } from "./Conversation.js"
+import mongoose from "mongoose";
+
+const userSchema = mongoose.Schema(
+    {
+        name: {
+            type: String,
+            required: true,
+        },
+        email: {
+            type: String,
+            required: true,
+        },
+        password: {
+            type: String,
+            required: true
+        },
+        description: {
+            type: String,
+            required: true
+        },
+        listing:  {
+            type: mongoose.Types.ObjectId,
+            ref: Listing
+        },
+        internship: {
+            type: mongoose.Types.ObjectId,
+            ref: Internship
+        }
+        /*conversations: {
+            type: [mongoose.Types.ObjectId],
+            ref: Conversation
+        }*/
+        // not 100% sure how to do this
+    },
+    {
+        timestamps: true,
+    }
+)
+
+export const User = mongoose.model('User', userSchema);
