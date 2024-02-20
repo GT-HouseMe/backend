6fe40e360cb3abf9911cdd06f3f9fc6d127f0fed
Mokshith <86264800+MMannuru@users.noreply.github.com>
2024-02-20 01:44:12 -0500

    Update Listing.js

--- models/Listing.js
+++ models/Listing.js
@@ -1,1 +1,37 @@
 // User apartment listing (includes location, rent, start date, end datae, description)
+import mongoose from "mongoose";
+
+const listingSchema = mongoose.Schema(
+  {
+    location: {
+      type: String,
+      required: true,
+    },
+    rent: {
+      type: Number,
+      required: true,
+    },
+    startDate: {
+      type: Date,
+      required: true,
+    },
+    endDate: {
+      type: Date,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    createdBy: {
+      ref: User,
+      type: mongoose.Types.ObjectId,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export const Listing = mongoose.model("Listing", listingSchema);
