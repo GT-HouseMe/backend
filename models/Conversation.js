0a5ee35067e24db5952cb24b47160c05f536407d
Amy <45153739+amtang325@users.noreply.github.com>
2024-02-20 18:54:27 -0500

    finished conversation and message schemas

--- models/Conversation.js
+++ models/Conversation.js
@@ -1,1 +1,21 @@
 // Conversation between users (includes conversation id, users involved, date created, etc.)
+const mongoose = require('mongoose');
+const User = require('./User.js');
+
+const conversationSchema = mongoose.Schema(
+    {
+        dateCreated: {
+            type: Date,
+            required: true,
+        },
+        members: [{
+            type: mongoose.Types.ObjectId,
+            ref: User
+        }]
+    },
+    {
+        timestamps: true,
+    }
+)
+
+module.exports = mongoose.model('Conversation', conversationSchema);
--- models/Message.js
+++ models/Message.js
@@ -1,1 +1,28 @@
 // A message in a conversation (includes sent by, timestamp, id of conversation it belongs to, message contents, etc.)
+const mongoose = require('mongoose');
+const User = require('./User.js');
+const Conversation = require('./Conversation.js');
+
+const messageSchema = mongoose.Schema(
+    {
+        sentBy: {
+            type: mongoose.Types.ObjectId,
+            ref: User,
+            required: true,
+        },
+        conversation: {
+            type: mongoose.Types.ObjectId,
+            ref: Conversation,
+            required: true,
+        },
+        content: {
+            type: String,
+            required: true,
+        }
+    },
+    {
+        timestamps: true,
+    }
+)
+
+module.exports = mongoose.model('Message', messageSchema);
