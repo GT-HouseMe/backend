e253cc1044f9c14e52b4c6a4cff49fffc2db1f54
Joseph Campbell <campbellbjoseph@gmail.com>
2024-03-07 03:43:18 -0500

    Added user auth 1.0, getting error on postman

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -1,13 +1,63 @@
 const express = require('express')
-const User = ('../models/userModel.js');
+const User = ('../models/User.js');
 const router = express.Router();
 
 router.post('/register', async(request, response) => {
-
+    try {
+        if (
+          !request.body.name ||
+          !request.body.email ||
+          !request.body.password ||
+          !request.body.description
+        ) {
+          return response.status(400).send({
+            message: 'Send all required fields: name, email, password, description',
+          });
+        }
+        const newUser = {
+            name : request.body.name,
+            email : request.body.email,
+            password : request.body.password,
+            description : request.body.description,
+        };
+        
+        const user = await User.create(newUser);
+        const token = user.createJWT();
+        return response.status(201).json({
+          user: {name: user.name},
+          token
+        });
+      } catch (error) {
+        console.log(error.message);
+        response.status(500).send({ message: error.message });
+      }
 });
 
 router.post('/login', async(request, response) => {
+  try {
+    if (
+      !request.body.name ||
+      !request.body.email ||
+      !request.body.password ||
+      !request.body.description
+    ) {
+      return response.status(400).send({
+        message: 'Send all required fields: name, email, password, description',
+      });
+    }
+    
 
+    const user = await User.create(newUser);
+    const token = user.createJWT();
+    return response.status(201).json({
+      user: {name: user.name},
+      token
+    });
+    return null;
+  } catch (error) {
+    console.log(error.message);
+    response.status(500).send({ message: error.message });
+  }
 });
 
 module.exports = router;
