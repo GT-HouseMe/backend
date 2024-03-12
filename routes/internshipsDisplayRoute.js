a8df5f7d15c09a14284bae52d05dbdce9ac94229
Amy <45153739+amtang325@users.noreply.github.com>
2024-03-12 18:26:53 -0400

    added listing and internship display routes

---
+++ routes/internshipsDisplayRoute.js
@@ -0,0 +1,32 @@
+const Internship = require('../models/Internship.js');
+const express = require('express');
+const router = express.Router();
+
+// Route for Getting All Internships from the database
+router.get('/', async (request, response) => {
+    try {
+      const internships = await Internship.find({});
+  
+      return response.status(200).json({
+        count: internships.length,
+        data: internships,
+      });
+    } catch (error) {
+      console.log(error.message);
+      response.status(500).send({ message: error.message });
+    }
+  });
+  
+  // Route for Getting One Internship from database by its id
+  router.get('/:id', async (request, response) => {
+    try {
+      const { id } = request.params;
+  
+      const internship = await Internship.findById(id);
+  
+      return response.status(200).json(internship);
+    } catch (error) {
+      console.log(error.message);
+      response.status(500).send({ message: error.message });
+    }
+  });
---
+++ routes/listingsDisplayRoute.js
@@ -0,0 +1,32 @@
+const Listing = require('../models/Listing.js');
+const express = require('express');
+const router = express.Router();
+
+// Route to get all listings from our database
+router.get('/', async (request, response) => {
+    try {
+      const listings = await Listing.find({});
+  
+      return response.status(200).json({
+        count: listings.length,
+        data: listings,
+      });
+    } catch (error) {
+      console.log(error.message);
+      response.status(500).send({ message: error.message });
+    }
+  });
+  
+  // Route to only get one listing by its ID from our database
+  router.get('/:id', async (request, response) => {
+    try {
+      const { id } = request.params;
+  
+      const listing = await Listing.findById(id);
+  
+      return response.status(200).json(listing);
+    } catch (error) {
+      console.log(error.message);
+      response.status(500).send({ message: error.message });
+    }
+  });
